052f532f248df44e5b84ea0f96d84bf9b0063b01
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2025-02-07 12:14:38 +0500

    написан тест в котором проверяем, загружжается ли контекст

---
+++ src/test/java/ProjectServiceApplicationTests.java
@@ -0,0 +1,14 @@
+import faang.school.projectservice.ProjectServiceApplication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ProjectServiceApplication.class)
+public class ProjectServiceApplicationTests {
+
+    @Test
+    @DisplayName("Test context loading")
+    void contextLoads() {
+
+    }
+}
