ae7221f8a1563654e3220c7af3bb479d44cb46c3
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2025-02-21 20:54:53 +0500

    Написал фичу для загрузки файлов в проекты и тесты к ним

--- src/main/java/faang/school/projectservice/repository/TeamMemberRepository.java
+++ src/main/java/faang/school/projectservice/repository/TeamMemberRepository.java
@@ -1,20 +1,26 @@
 package faang.school.projectservice.repository;
 
 import faang.school.projectservice.model.TeamMember;
+import faang.school.projectservice.model.TeamRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
     @Query(
-        "SELECT tm FROM TeamMember tm JOIN tm.team t " +
-        "WHERE tm.userId = :userId " +
-        "AND t.project.id = :projectId"
+            "SELECT tm FROM TeamMember tm JOIN tm.team t " +
+                    "WHERE tm.userId = :userId " +
+                    "AND t.project.id = :projectId"
     )
     TeamMember findByUserIdAndProjectId(long userId, long projectId);
 
     List<TeamMember> findByUserId(long userId);
 
     TeamMember findSingleByUserId(long userId);
+
+    @Query("SELECT tm.roles FROM TeamMember tm WHERE tm.id = :userId")
+    List<TeamRole> findUserRolesByUserId(@Param("userId") Long userId);
+
 }
